27663cb70ddbd06585b8dfc6d9a376daee1bd70c
birukCode <birukyenewk@gmail.com>
2026-06-16 02:59:38 -0400

    Applied custom loss masking for error detection. Model only judged based on last token (0 or 1)

--- nanoGPT/train.py
+++ nanoGPT/train.py
@@ -114,8 +114,7 @@ ctx = nullcontext() if device_type == 'cpu' else torch.amp.autocast(device_type=
 # poor man's data loader
 data_dir = os.path.join('data', dataset)
 def get_batch(split):
-    # We recreate np.memmap every batch to avoid a memory leak, as per
-    # https://stackoverflow.com/questions/45132940/numpy-memmap-memory-usage-want-to-iterate-once/61472122#61472122
+    
     if split == 'train':
         data = np.memmap(os.path.join(data_dir, 'train.bin'), dtype=np.uint16, mode='r')
     else:
@@ -124,20 +123,12 @@ def get_batch(split):
     x = torch.stack([torch.from_numpy((data[i:i+block_size]).astype(np.int64)) for i in ix])
     y = torch.stack([torch.from_numpy((data[i+1:i+1+block_size]).astype(np.int64)) for i in ix])
     if device_type == 'cuda':
-        # pin arrays x,y, which allows us to move them to GPU asynchronously (non_blocking=True)
         x, y = x.pin_memory().to(device, non_blocking=True), y.pin_memory().to(device, non_blocking=True)
     else:
         x, y = x.to(device), y.to(device)
     
-    # --- CUSTOM MASKING LOGIC FOR TARGET LOSS ---
-    # Token ID 3 is '>' and Token ID 2 is '='
-    # We set target values to -1 up to the answer marker so PyTorch ignores them in CrossEntropy Loss
-    
-    for b in range(y.size(0)):
-        delimiter_indices = ((x[b] == 3) | (x[b] == 2)).nonzero(as_tuple=True)[0]
-        if len(delimiter_indices) > 0:
-            cutoff = delimiter_indices[-1].item() + 1
-            y[b, :cutoff] = -1
+    #The model is only evaluatedon the last token (0 or 1).
+    y[:, :-1] = -1
 
     return x, y
 
